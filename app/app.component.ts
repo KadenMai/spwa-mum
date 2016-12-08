fea32476228130f036f58baf102ed1d0383044e0
Kaden Mai <kaden.mai@hotmail.com>
2016-12-08 14:28:14 -0600

    2. The Product Editor
    - wrote a multi-line template using ES2015’s template
    -  can both display and change the product's name after adding a two-way
    data binding to the <input> element using the built-in ngModel
    directive.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,28 @@
 import { Component } from '@angular/core';
 
+export class Product{
+  id: number;
+  name: string;
+}
+
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `
+<h1>{{title}}</h1>
+<h2>{{product.name}} details!</h2>
+<div><label>id:</label>{{product.id}}</div>
+<div>
+  <label>name: </label>
+  <input [(ngModel)]="product.name" placeholder="name">
+</div>
+
+`,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  {
+
+  title = 'List of Products';
+  product: Product = {
+    id: 1,
+    name: 'Iphone'
+  }
+}
--- dist/app/app.component.js
+++ dist/app/app.component.js
@@ -9,16 +9,26 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require("@angular/core");
+var Product = (function () {
+    function Product() {
+    }
+    return Product;
+}());
+exports.Product = Product;
 var AppComponent = (function () {
     function AppComponent() {
-        this.name = 'Angular';
+        this.title = 'List of Products';
+        this.product = {
+            id: 1,
+            name: 'Iphone'
+        };
     }
     return AppComponent;
 }());
 AppComponent = __decorate([
     core_1.Component({
         selector: 'my-app',
-        template: "<h1>Hello {{name}}</h1>",
+        template: "\n<h1>{{title}}</h1>\n<h2>{{product.name}} details!</h2>\n<div><label>id:</label>{{product.id}}</div>\n<div>\n  <label>name: </label>\n  <input [(ngModel)]=\"product.name\" placeholder=\"name\">\n</div>\n\n",
     }),
     __metadata("design:paramtypes", [])
 ], AppComponent);
